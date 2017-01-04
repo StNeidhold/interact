079f191d3fb63dae170a87a21fdd52332b905f69
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:53:34 +0000

    Direct association between firms and recruiting_events

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -1,6 +1,9 @@
 class Firm < ApplicationRecord
   # Direct associations
 
+  has_many   :recruiting_events,
+             :dependent => :destroy
+
   has_many   :banking_titles,
              :dependent => :destroy
 
