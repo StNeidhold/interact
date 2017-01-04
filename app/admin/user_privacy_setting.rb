7a49be574a71c3f3f01b78d0b32e6bbe51f4c2dc
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:55:41 +0000

    Whitelist columns in UserPrivacySetting for ActiveAdmin

--- app/admin/user_privacy_setting.rb
+++ app/admin/user_privacy_setting.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserPrivacySetting do
 
+ permit_params :user_id, :share_contacts, :share_interactions, :share_events
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
