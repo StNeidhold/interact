56bce3e3a3e65c5309f775a0805338d84e628e6e
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:54:29 +0000

    Whitelist columns in UserGroup for ActiveAdmin

--- app/admin/user_group.rb
+++ app/admin/user_group.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserGroup do
 
+ permit_params :group_access_setting_id, :name, :description, :group_privacy_setting_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
