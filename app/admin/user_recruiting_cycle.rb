6b261539f07cd394cd49707e488aade297280a76
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:55:14 +0000

    Whitelist columns in UserRecruitingCycle for ActiveAdmin

--- app/admin/user_recruiting_cycle.rb
+++ app/admin/user_recruiting_cycle.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserRecruitingCycle do
 
+ permit_params :user_id, :start_date, :end_date, :name, :is_active
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
