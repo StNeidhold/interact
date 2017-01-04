d8d05f908a3e8a1135cf39fa3fae50d0e6096da9
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:53:34 +0000

    Direct association between user_groups and user_group_members

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,6 +1,9 @@
 class UserGroup < ApplicationRecord
   # Direct associations
 
+  has_many   :user_group_members,
+             :dependent => :destroy
+
   has_many   :group_join_requests,
              :dependent => :destroy
 
--- app/models/user_group_member.rb
+++ app/models/user_group_member.rb
@@ -1,6 +1,8 @@
 class UserGroupMember < ApplicationRecord
   # Direct associations
 
+  belongs_to :user_group
+
   belongs_to :user
 
   # Indirect associations
