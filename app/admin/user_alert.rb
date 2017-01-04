c596006538fa436ab18b0a0a2554b9ceb950047b
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:56:04 +0000

    Whitelist columns in UserAlert for ActiveAdmin

--- app/admin/user_alert.rb
+++ app/admin/user_alert.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserAlert do
 
+ permit_params :user_id, :alert_text
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
