07d90f4d981ddd03dfec0e5516abcea79ae01888
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:54:47 +0000

    Whitelist columns in BankingTitle for ActiveAdmin

--- app/admin/banking_title.rb
+++ app/admin/banking_title.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BankingTitle do
 
+ permit_params :firm_id, :title
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
