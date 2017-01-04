221e52a3fc634986f34ce2feff0d593c8cef6348
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:53:34 +0000

    Direct association between users and event_rsvps

--- app/models/event_rsvp.rb
+++ app/models/event_rsvp.rb
@@ -1,6 +1,8 @@
 class EventRsvp < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :event_rsvps,
+             :dependent => :destroy
+
   has_many   :user_alerts,
              :dependent => :destroy
 
