7b592473d781586a7eba9d590df0b1e3fea972d2
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:55:52 +0000

    Whitelist columns in UserQuestionBank for ActiveAdmin

--- app/admin/user_question_bank.rb
+++ app/admin/user_question_bank.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserQuestionBank do
 
+ permit_params :user_id, :question_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
