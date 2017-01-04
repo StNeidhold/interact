e86a6b41f9284abf1191862ff645e56b5ee9070a
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:53:34 +0000

    Direct association between cities and offices

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,9 @@
 class City < ApplicationRecord
   # Direct associations
 
+  has_many   :offices,
+             :dependent => :nullify
+
   # Indirect associations
 
   # Validations
--- app/models/office.rb
+++ app/models/office.rb
@@ -1,6 +1,8 @@
 class Office < ApplicationRecord
   # Direct associations
 
+  belongs_to :city
+
   belongs_to :firm
 
   # Indirect associations
