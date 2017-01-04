c64728298ef5e912a03d578de60eb8a03ae31d0a
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:55:48 +0000

    Whitelist columns in InteractionQuestion for ActiveAdmin

--- app/admin/interaction_question.rb
+++ app/admin/interaction_question.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register InteractionQuestion do
 
+ permit_params :interaction_id, :question_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
