442f122195da271955a27ed3006dc3e1072c5169
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:54:11 +0000

    Whitelist columns in Firm for ActiveAdmin

--- app/admin/firm.rb
+++ app/admin/firm.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Firm do
 
+ permit_params :name, :process_type_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
