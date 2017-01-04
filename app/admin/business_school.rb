89b86fdb29b1c744e42d4613273596a85b6b9a22
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:54:52 +0000

    Whitelist columns in BusinessSchool for ActiveAdmin

--- app/admin/business_school.rb
+++ app/admin/business_school.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BusinessSchool do
 
+ permit_params :business_school_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
