e6ad2bba1f2058adfd27b35cbc674fbf53931307
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:55:26 +0000

    Whitelist columns in GroupAccessSetting for ActiveAdmin

--- app/admin/group_access_setting.rb
+++ app/admin/group_access_setting.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register GroupAccessSetting do
 
+ permit_params :acessibility, :visibility
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
