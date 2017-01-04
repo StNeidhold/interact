b74650c689e3666c6856f0a2fa1ffb63fd3d7be6
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:53:51 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "interactions#index"
   # Routes for the Firm_recruiting_cycle resource:
   # CREATE
