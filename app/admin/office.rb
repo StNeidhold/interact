657919ee23b0b22074557520569991cbe173dfcd
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:54:16 +0000

    Whitelist columns in Office for ActiveAdmin

--- app/admin/office.rb
+++ app/admin/office.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Office do
 
+ permit_params :firm_id, :city_id, :firm_hq
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
