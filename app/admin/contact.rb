751b7008ceacdd61671f46b2697030fe4ad57ccb
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:54:06 +0000

    Whitelist columns in Contact for ActiveAdmin

--- app/admin/contact.rb
+++ app/admin/contact.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Contact do
 
+ permit_params :first_name, :last_name, :banking_title_id, :firm_id, :gender_id, :college_id, :business_school_id, :firm_group_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
