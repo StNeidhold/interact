a2cca5aa7a416b5d78cf6779d35af3ae34e846e0
Raghu Betina <raghu@firstdraft.com>
2017-01-04 14:55:09 +0000

    Whitelist columns in Gender for ActiveAdmin

--- app/admin/gender.rb
+++ app/admin/gender.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Gender do
 
+ permit_params :gender
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
